3a0d28596d54ec091ed49d03d2137983a7a63894
JosephChennattu123 <joch00001@uni-saarland.de>
2023-06-09 23:39:58 +0200

    Ima go out on a date wish me luck guys

--- src/uttt/tests/BoardTests.java
+++ src/uttt/tests/BoardTests.java
@@ -5,7 +5,7 @@ import uttt.utils.Symbol;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import junit.*;
+
 import uttt.UTTTFactory;
 
 public class BoardTests {
@@ -20,47 +20,61 @@ public class BoardTests {
     }
 
     @Test
-    public void testSetMarkAt() {
-        b = board.setMarkAt(Symbol.CIRCLE, 4);
-        assertTrue(b);
-        MarkInterface[] marks = board.getMarks();
-        assertEquals(Symbol.CIRCLE, marks[4].getSymbol());
-    }
-
+    public void TestgetMarks()
+{
+    p= board.getMarks();
+    assertNotNull(p);
+    assertEquals(81,p.length);
+}   
     @Test
-    public void testIsClosed() {
-        // test when the board is not closed
-        assertFalse(board.isClosed());
-
-        // ... Set the board to a closed state
-
-        // test when the board is closed
-        assertTrue(board.isClosed());
-    }
-
-    @Test
-    public void testIsMovePossible() {
-        // set 0 to empty
-        board.setMarkAt(Symbol.EMPTY, 0);
-        assertTrue(board.isMovePossible(0));
-
-        // mark 0 with cross
-        board.setMarkAt(Symbol.CROSS, 0);
-        assertFalse(board.isMovePossible(0));
-    }
-
-    @Test
-    public void testGetWinner() {
+    public void TestsetMarks()
+    {
         p = new MarkInterface[81];
         board.setMarks(p);
-        assertEquals(Symbol.EMPTY, board.getWinner());
-
-        // Set the board to a winning state for CROSS
-        for (int i = 0; i < 81; i++) {
-            b = board.setMarkAt(Symbol.CROSS, i);
-            assertTrue(b);
+        MarkInterface[] neueMarks = board.getMarks();
+        assertArrayEquals(p,neueMarks);
         }
-        assertEquals(Symbol.CROSS, board.getWinner());
+    
+
+    @Test
+    public void TestsetMarkAt(){
+       b = board.setMarkAt(Symbol.CIRCLE,4);
+        assertTrue(b);
+        MarkInterface[] marks = board.getMarks();
+        assertEquals(Symbol.CIRCLE,marks[4].getSymbol());
+    }
+    @Test
+    public void TestisClosed(){
+        
+        //test when board not closed
+        assertFalse(board.isClosed());
+        //set board to a closed state
+        assertTrue(board.isClosed());
+        
+
+    }
+    @Test
+    public void TestisMovePossible(){
+        //set 0 to empty
+        board.setMarkAt(Symbol.EMPTY, 0)
+        assertTrue(board.isMovePossible(0));
+        //mark 0 with cross
+        board.setMarkAt(Symbol.CROSS,0);
+        assertFalse(board.isMovePossible(0));
+    }
+    @Test
+    public void TestgetWinner()
+    {
+        p = new MarkInterface[81];          
+        board.setMarks(p);
+        assertEquals(Symbol.EMPTY,board.getWinner());
+        //Set board to winning state for CROSS
+        for(int i =0;i<81;i++)
+        {
+        b = board.setMarkAt(Symbol.CROSS, i);
+        assertTrue(b);
+        }
+        assertEquals(Symbol.CROSS,board.getWinner());
     }
 
 }
