722b7ffbd8b1ffffc896f2b88e42d658f193cc8c
JosephChennattu123 <joch00001@uni-saarland.de>
2023-06-16 21:55:15 +0200

    holy shit fam

--- src/uttt/game/MarkInterfaceImplementation.java
+++ src/uttt/game/MarkInterfaceImplementation.java
@@ -6,25 +6,33 @@ public class MarkInterfaceImplementation implements MarkInterface {
     private int position;
     private Symbol symbol;
     
-    public MarkInterfaceImplementation(int position)
+    public MarkInterfaceImplementation(Symbol simbol, int position)
     {
-        this.symbol = Symbol.EMPTY;
+        this.symbol = simbol;
         this.position = position;
     }
 
     @Override
     public Symbol getSymbol() {
-       return symbol;
+       return this.symbol;
     }
 
     @Override
-    public int getPosition() {
-        return position;
+    public int getPosition() throws IllegalArgumentException {
+        if(position<9 && position>-1)
+        return this.position;
+        else 
+        throw new IllegalArgumentException("wrong bye bye");
     }
 
     @Override
-    public void setSymbol(Symbol symbol) throws IllegalArgumentException {
-        this.symbol = symbol;
+    public void setSymbol(Symbol simbol) throws IllegalArgumentException {
+        
+        if(simbol== null)
+        throw new IllegalArgumentException("null hai");
+        if(simbol!= Symbol.CIRCLE || simbol!=Symbol.CROSS || simbol!=Symbol.EMPTY)
+        throw new IllegalArgumentException("wrong symbol");
+        this.symbol = simbol;
     }
     
 }
